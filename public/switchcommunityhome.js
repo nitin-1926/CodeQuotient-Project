fa467782cde9f1283cf337951721891b63f4f7d9
Nitin Gupta <gupta7nitin@gmail.com>
2019-10-03 11:30:10 +0530

    Request Accepted

--- public/switchcommunityhome.js
+++ public/switchcommunityhome.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     var p = document.getElementById("objectId")
     p = p.textContent;
+    var flag=0;
     var request1 = new XMLHttpRequest()
     request1.open('GET','/ownedCommunities');
     request1.send();
@@ -8,13 +9,26 @@ $(document).ready(function() {
     {
       var data = JSON.parse(request1.responseText);
       for(var i in data)
-      {
+      { //console.log("Major")
+        flag=0;
         if(data[i].communityownerid == p)
         addtoDOM1(data[i]);
         else if(data[i].communitymembershiprule == "Direct")
         addtoDOM2(data[i]);
-        else if(data[i].communitymembershiprule == "Permission")
-        addtoDOM3(data[i]);
+        else if(data[i].communitymembershiprule == "Permission"){
+            for(var j in data[i].communityrequest){
+              if(p == data[i].communityrequest[j]){
+                flag=1;
+                break;
+              }
+            }
+            if(flag==0){
+              addtoDOM2(data[i]);
+            }
+            else{
+              addtoDOM3(data[i]);
+            }
+        }
       }
     }
 })
@@ -45,6 +59,7 @@ function addtoDOM3(obj)
   var div = '<div class="col-sm-12 col-xs-12 community-div" id="'+obj._id+'" style="margin-top:5px;"><div class="col-sm-1 col-xs-3" style="padding:10px;z-index:1"><a href="#"><img src="'+obj.communityimage+'" class="cpic"></a></div><div class="col-sm-10 col-xs-7" style="padding-top:25px;padding-bottom:5px;"><p style="margin:0"><a style="text-decoration:none;" href="#"><label class="label label-danger">Pending</label>&nbsp;&nbsp;&nbsp;'+obj.communityname+'</a>&nbsp;&nbsp;&nbsp;<a style="text-decoration:none;color:black;cursor:context-menu">Members('+obj.communitymember.length+')</a></p></div><div class="col-sm-1 col-xs-2" style="padding:0"><a class="community-short-btn" data-toggle="modal" data-target="#cancelRequest" onclick=cancelRequest("'+obj._id+'") style="float:right"><label class="label label-danger" style="cursor:pointer !important;"><i class="fa fa-times"></i></label></a></div></div>'
 
    $("#request-list").append(div);
+   console.log(obj);
 }
 
 function cancelRequest(_id)
